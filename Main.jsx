94d0cc22602d623b33f82839c508b5b609608701
andyrhman <andyrahman989@gmail.com>
2024-05-06 20:28:39 +0800

    pushed to repo

---
+++ Main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
+
+const Main = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Group>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Main;
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# First Time Configuration
+
+```bash
+mkdir myapp
+cd myapp
+npx create-expo-app .
+```
+
+## Install some dependencies
+
+```bash
+npm i @react-navigation/native @react-navigation/native-stack react-native-paper
+```
